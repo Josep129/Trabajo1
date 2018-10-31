889591bd9ba2c079c48e4345d64c582df17ee303
Josep129 <josep129h@gmail.com>
2018-10-30 19:43:11 -0500

    agregandole cuerpo

--- Interfaz.py
+++ Interfaz.py
@@ -10,6 +10,21 @@ def abrirventana():
     ventana.withdrawn()
     win=tk.Toplevel()
     win.geometry()
-    win.configure(background,"dark turqouise")
+    win.configure(background,'dark turqouise')
     e3=tk.Label(win,text="Segunda ventana",bg="pink",fg="white")
-    e3.pack(padx=5,padv=5)
+    e3.pack(padx=5,padv=5,ipadx=5,ipadv=5,fill=tk.x)
+
+    boton2=tk.Button(win,text='ok',command=win.destroy)
+    boton2.pack(side=tk.TOP)
+
+def cerrarventana():
+    ventana.destroy()
+
+ventana=tk.Tk()
+ventana.title("Ventana 1")
+ventana.geometry('380x300')
+ventana.configure(background= 'dark turquoise')
+
+el = tk.Label(ventana,text="Password:",bg="pink",fg="white")
+el.pack(padx=5,padv=5,ipadx=5,ipadv=5)
+boton=tk.Button(ventana,text="Nueva Ventana",fg="blue",commmand=abrirventana)
