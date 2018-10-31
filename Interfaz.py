cf9adfd06349defd9935492df2e802973a98b91e
alumno informatica <alumno@ciis.usil.edu.pe>
2018-10-31 08:15:37 -0500

    cuerpo terminado

--- Interfaz.py
+++ Interfaz.py
@@ -1,18 +1,19 @@
-from tkinter import *
+import tkinter as tk
+from tkinter import messagebox
 
 def validar():
-    if entrada1.get()=="josep129":
-        abrirventana()
+    if entrada1.get()=='josep129':
+        abrirventana2()
     else:
         messagebox.showwarning("Cuidado","Password Incorrecto")
 
-def abrirventana():
+def abrirventana2():
     ventana.withdrawn()
     win=tk.Toplevel()
-    win.geometry()
-    win.configure(background,'dark turqouise')
+    win.geometry('380x300+1900+100')
+    win.configure(background='dark turqouise')
     e3=tk.Label(win,text="Segunda ventana",bg="pink",fg="white")
-    e3.pack(padx=5,padv=5,ipadx=5,ipadv=5,fill=tk.x)
+    e3.pack(padx=5,padv=5,ipadx=5,ipadv=5,fill=tk.X)
 
     boton2=tk.Button(win,text='ok',command=win.destroy)
     boton2.pack(side=tk.TOP)
@@ -26,6 +27,12 @@ ventana.geometry('380x300')
 ventana.configure(background= 'dark turquoise')
 
 el = tk.Label(ventana,text="Password:",bg="pink",fg="white")
-el.pack(padx=5,padv=5,ipadx=5,ipadv=5)
-boton=tk.Button(ventana,text="Nueva Ventana",fg="blue",commmand=abrirventana)
+el.pack(padx=5,pady=5,ipadx=5,ipady=5)
+entrada1=tk.Entry(ventana)
+entrada1.pack(fill=tk.X, padx=5, pady=5, ipadx=5, ipady=5)
+boton=tk.Button(ventana,text="Nueva Ventana",fg="blue",command=abrirventana2)
+boton.pack(side=tk.TOP)
+boton3=tk.Button(ventana,text="Validar Password",fg="blue",command=abrirventana2)
+boton3.pack(side=tk.TOP)
 
+ventana.mainloop()
